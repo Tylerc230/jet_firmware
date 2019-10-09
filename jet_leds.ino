2c8e452a57b8fbdaeb47bd09a6e042ec543f4f80
Tyler Casselman <tylerc230@gmail.com>
2019-10-09 16:14:50 -0700

    Reading all 10 switches

--- jet_leds.ino
+++ jet_leds.ino
@@ -1,9 +1,30 @@
+#include <FastLED.h>
+const int switchPins[] = {2,3,4,6,7,8,9,10,11,12};
+const int switchCount = sizeof(switchPins)/sizeof(switchPins[0]);
+int switchState [switchCount];
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  initSwitches();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readSwitches();
+  for (int i = 0; i < switchCount; i++) {
+    Serial.print(switchState[i]);
+  }
+  Serial.println();
+}
 
+void readSwitches() {
+  for (int i = 0; i < switchCount; i++) {
+    int state = digitalRead(switchPins[i]);
+    switchState[i] = state;
+  }
+}
+
+void initSwitches() {
+  for (int i = 0; i < switchCount; i++) {
+    pinMode(switchPins[i], INPUT_PULLUP);
+    digitalWrite(switchPins[i], HIGH);
+  }
 }
