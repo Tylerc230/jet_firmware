997d942beeb09e82472b6643a313362fb79f2f88
Tyler Casselman <tylerc230@gmail.com>
2019-10-27 00:12:56 -0700

    Update to proper led counts etc

--- constants.h
+++ constants.h
@@ -1,9 +1,9 @@
 #define BRIGHTNESS  64
-#define LED_TYPE    WS2811
+#define LED_TYPE    WS2812B	
 #define COLOR_ORDER GRB
 #define UPDATES_PER_SECOND 100
 
-#define MAX_LEDS 75
+#define MAX_LEDS 30
 #define L_NOSE_LED_PIN 2
 #define R_NOSE_LED_PIN 3
 #define L_WING_LED_PIN 4
@@ -19,7 +19,7 @@
 #define KNOB_B_PIN_0 12
 #define KNOB_B_PIN_1 13
 
-#define NOSE_LED_COUNT 50
-#define WING_LED_COUNT 50
-#define TAIL_LED_COUNT 50
+#define NOSE_LED_COUNT 27
+#define WING_LED_COUNT 29
+#define TAIL_LED_COUNT 30
 
